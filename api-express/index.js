2614a7c9fa6b29f3c9caeffaba00f888457ed6a5
mohamedouerfellii <mohamedouerfelli3@gmail.com>
2025-12-01 18:19:37 +0100

    swagger

--- api-express/index.js
+++ api-express/index.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const profs = require("./data/profs");
 const profsRouter = require('./routers/profsRouter').router;
 const messagesRouter = require('./routers/messagesRouter').router;
-// console.log(profs.getAll());
-// console.log(profs.getById(187));
-// console.log(profs.getById(1));
-// profs.add("Ouerfelli", "Mohamed", "220");
-// console.log(profs.getAll());
-// console.log(profs.deleteById(187));
-// console.log(profs.deleteById(1));
-// console.log(profs.getAll());
-// console.log(profs.update(188, "Ouerfelli", "Mohamed", "221"));
-// console.log(profs.getAll());
+
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+
+const expressOasGenerator = require('express-oas-generator');
 
 const app = express();
+
+expressOasGenerator.handleResponses(app, {});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const accessLogStream = rfs.createStream('access.log', {
+    interval: '1d',
+    path: path.join(__dirname, 'logs'),
+});
+app.use(morgan('combined', {stream: accessLogStream}));
 
 app.use('/api/v1', profsRouter);
 app.use('/api/v1', messagesRouter);
@@ -24,6 +28,8 @@ app.use('/api/v1', messagesRouter);
 app.get("/", function (req, res) {
     res.send("API REST avec Express");
 });
+
+expressOasGenerator.handleRequests();
 
 app.listen(8080, () => {
     console.log("listening on port 8080");
